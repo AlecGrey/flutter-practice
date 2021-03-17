42b6d090c0e4f7ee365de0da5b33ef79c3050a8e
AlecGrey <alecgrey.coding@gmail.com>
2021-03-17 15:16:41 -0700

    migrated quiz to separate file, incorporated routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,49 @@
 // packages
 import 'package:flutter/material.dart';
 // widgets
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _index = 0;
-
-  void _answerQuestion() {
-    print(_index);
-    setState(() => _index++);
-  }
-
-  bool _questionsRemaining() {
-    return _questions.length > _index;
-  }
-
-  static const _questions = [
-    {
-      'questionText': "What is your name?",
-      'answers': ['steve', 'bo', 'derpo', 'fleeb'],
-    },
-    {
-      'questionText': "What is your quest?",
-      'answers': [
-        'To find the Holy Grail',
-        'Measure the airspeed velocity of an African Swallow',
-        'To bring a shrubbery',
-        'To bravely run away'
-      ],
-    },
-    {
-      'questionText': "What is your favorite color?",
-      'answers': ['Blue', 'Green', 'Fuchsia', 'Yellow'],
-    }
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'App',
-          ),
-          backgroundColor: Colors.blueGrey[700],
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  // NAVIGATE TO QUIZ
+  void navigateToQuiz(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Quiz();
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'App',
         ),
-        body: _questionsRemaining()
-            ? Column(
-                children: [
-                  Question(
-                    _questions[_index]['questionText'],
-                  ),
-                  ...(_questions[_index]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
-              )
-            : Center(
-                child: Text('No questions remain.',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                    )),
-              ),
+        backgroundColor: Colors.blueGrey[700],
+      ),
+      body: Container(
+        margin: EdgeInsets.all(24),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text('Welcome Home!'),
+            ElevatedButton(
+              onPressed: () => navigateToQuiz(context),
+              child: Text('Launch Quiz!'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -6,14 +6,82 @@ import './answer.dart';
 
 class Quiz extends StatefulWidget {
   @override
-  _QuizState createState() => _QuizState();
+  State<StatefulWidget> createState() {
+    return _QuizState();
+  }
 }
 
 class _QuizState extends State<Quiz> {
+  var _index = 0;
+
+  void _answerQuestion() {
+    print(_index);
+    setState(() => _index++);
+  }
+
+  bool _questionsRemaining() {
+    return _questions.length > _index;
+  }
+
+  static const _questions = [
+    {
+      'questionText': "What is your name?",
+      'answers': ['Arthur', 'Lancelot', 'Robin', 'Bedivere', 'Gallahad'],
+    },
+    {
+      'questionText': "What is your quest?",
+      'answers': [
+        'To find the Holy Grail',
+        'Measure the airspeed velocity of an African Swallow',
+        'To bring a shrubbery',
+        'To bravely run away'
+      ],
+    },
+    {
+      'questionText': "What is your favorite color?",
+      'answers': ['Blue', 'Green', 'Fuchsia', 'Yellow'],
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: // INSERT CHILD HERE
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'App',
+          ),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        body: _questionsRemaining()
+            ? Column(
+                children: [
+                  Question(
+                    _questions[_index]['questionText'],
+                  ),
+                  ...(_questions[_index]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Column(
+                children: [
+                  Text(
+                    'Quiz Complete!.',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('Home'),
+                  ),
+                ],
+              )),
+      ),
     );
   }
 }
